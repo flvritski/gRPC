3520b3c6e066948d6a7dae80904660c1fdf7aee3
florin-stefan <florin.stefan@ibm.com>
2023-09-22 00:55:21 +0300

    section 6: gRPC Server Streaming prime exercise

---
+++ calculator/client/prime_many_times.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/flvritski/gRPC/calculator/proto"
+)
+
+func doPrimeManyTimes(c pb.CalculatorServiceClient) {
+	log.Println("doPrimeManyTimes was invoked")
+
+	req := &pb.PrimeRequest{
+		PrimeNumber: 1,
+	}
+
+	stream, err := c.PrimeManyTimes(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Error while calling PrimeManyTimes: %v\n", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading the stream %v\n", err)
+		}
+
+		log.Printf("PrimeManyTimes: %d\n", msg.PrimeResult)
+	}
+}
---
+++ calculator/server/prime_many_times.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+
+	pb "github.com/flvritski/gRPC/calculator/proto"
+)
+
+func (s *Server) PrimeManyTimes(in *pb.PrimeRequest, stream pb.CalculatorService_PrimeManyTimesServer) error {
+	log.Printf("PrimeManyTimes function was invoked with: %v\n", in)
+
+	// number := in.PrimeNumber
+	// divisor := int32(2)
+
+	// for number > 1 {
+	// 	if number%divisor == 0 {
+	// 		stream.Send(&pb.PrimeResponse{
+	// 			PrimeResult: divisor,
+	// 		})
+
+	// 		number /= divisor
+	// 	} else {
+	// 		divisor++
+	// 	}
+	// }
+
+	for i := 0; i < 10; i++ {
+		if isPrime(in.PrimeNumber) == true {
+			stream.Send(&pb.PrimeResponse{
+				PrimeResult: 1,
+			})
+		}
+	}
+	return nil
+}
+
+func isPrime(n int32) bool {
+	if n <= 1 {
+		fmt.Println("number must be greater than 2.")
+		return false
+	}
+
+	if n <= 3 {
+		return true
+	}
+
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+
+	sqrtN := int(math.Sqrt(float64(n)))
+
+	var i int32
+	for i = 5; i <= int32(sqrtN); i += 6 {
+		if n%i == 0 || n%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
